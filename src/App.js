db8f8064c177e298a264a6e8f2c8d523831e60b2
Fujinoki, Rio <riofujinoki@my.unt.edu>
2025-02-19 23:58:34 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PomodoroTimer from "./components/PomodoroTimer";
+
+function App() {
+  return (
+    <div>
+      <PomodoroTimer />
+    </div>
+  );
+}
+
+export default App;
